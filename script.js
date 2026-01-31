d6fc3c1aa350ed5436bb1815eee227c042e31d30
cheemsislive <kalanithik2006@gmail.com>
2026-01-31 21:30:14 +0530

    new

--- script.js
+++ script.js
@@ -234,3 +234,34 @@ document.addEventListener('DOMContentLoaded', () => {
         icon.addEventListener('click', toggleMenu);
     });
 });
+
+/* CONTENT PROTECTION */
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+document.onkeydown = function (e) {
+    if (e.keyCode == 123) { // F12
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) { // Ctrl+Shift+I
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) { // Ctrl+Shift+C
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) { // Ctrl+Shift+J
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) { // Ctrl+U
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)) { // Ctrl+S
+        return false;
+    }
+}
+
+// Disable drag on images
+document.addEventListener('dragstart', function (event) {
+    if (event.target.tagName === 'IMG') {
+        event.preventDefault();
+    }
+});
